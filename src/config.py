fbe31c6aca4f734f34b842825ab0e10528acbbbc
nadilHesara <nadilhesara@gmail.com>
2026-02-04 23:57:30 +0530

    Configured paths do data folder

--- src/config.py
+++ src/config.py
@@ -1,1 +1,13 @@
+from pathlib import Path
+
 # This is the place to put File paths, hyperparameters, constants
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+DATA_DIR = PROJECT_ROOT/"data"
+RAW_DIR = DATA_DIR/"raw"
+PROCESSED_DIR = DATA_DIR/"processed"
+FEATURES_DIR = DATA_DIR/"features"
+
+PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+FEATURES_DIR.mkdir(parents=True, exist_ok=True)
